fd8e43d06205cdea3c53d544b5e0dfcf5f3b2ef3
asingo619 <87049165+asingo619@users.noreply.github.com>
2021-07-31 13:25:45 -0700

    Add files via upload

---
+++ Assignment 2/script.js
@@ -0,0 +1,19 @@
+console.log("How Can I help?");
+
+var fm1, fm2, fm3 = "";//Family Combos
+fm1 = 50;
+fm2 = 50;
+fm3 = 50;
+var vm1, vm2, vm3 ="";//Value Meals
+vm1 = 15;
+vm2 = 13;
+vm3 = 11;
+var mnm1, mnm2 = "";//Mix N Match
+mnm1 = 9;
+mnm2 = 8;
+
+function order(){
+
+var makeSelection = prompt("Enter Menu Item Choice")
+
+}
